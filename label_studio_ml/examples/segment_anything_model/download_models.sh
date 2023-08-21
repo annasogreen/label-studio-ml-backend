c5118cae34ce901c956d024cfa263f7b267859a8
annasogreen <141873367+annasogreen@users.noreply.github.com>
2023-08-21 15:55:06 -0700

    Update download_models.sh

--- label_studio_ml/examples/segment_anything_model/download_models.sh
+++ label_studio_ml/examples/segment_anything_model/download_models.sh
@@ -7,6 +7,8 @@ mkdir -p models
 # [ -f /app/models/sam_vit_h_4b8939.pth ] ||
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -P models/
 
+sleep 198
+
 # check if file `mobile_sam.pt` exists, otherwise download the model
 [ -f models/mobile_sam.pt ] ||
 wget https://github.com/ChaoningZhang/MobileSAM/raw/master/weights/mobile_sam.pt -P models/
